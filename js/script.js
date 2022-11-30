aa3c2b5bc62e7746240ee38b887b9f8e7929b9d3
shaydulovshahzod85@gmail.com <shaydulovshahzod85@gmail.com>
2022-11-30 21:22:03 +0500

    mobile menu interaktiv by javascript

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 let menu = document.getElementById("menu");
-menu.addEventListener("click", open);
-let sideBar = document.getElementsByClassName("side-bar") 
-function open() {
-  sideBar.style.display = "block";
-}
+let sideBar = document.getElementById('side');
+let closeBtn = document.getElementById('close');
+
+
+menu.addEventListener("click", function(){
+  sideBar.style.width = '220px';
+})
+
+
+closeBtn.addEventListener('click', function() {
+  sideBar.style.width = '0';
+})
